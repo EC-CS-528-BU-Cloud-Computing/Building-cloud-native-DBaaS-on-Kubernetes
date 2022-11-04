e40d8b17df1e58ed55c960da8d0e05f47e789570
Jingyu Su <jingyusu@bu.edu>
2022-11-04 12:56:17 -0400

    pod spawn

---
+++ tidbOp/pkg/spawn/pod.go
@@ -0,0 +1,37 @@
+package spawn
+
+import (
+	dbaasv1alpha1 "tidbOp/api/v1alpha1"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func NewPodForCR(cr *dbaasv1alpha1.Tidb) *corev1.Pod {
+	labels := map[string]string{
+		"app": cr.Name,
+	}
+
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name,
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "tidb",
+					Image: cr.Spec.Imagename,
+					Ports: []corev1.ContainerPort{
+						{
+							ContainerPort: 20160,
+						},
+					},
+					Args: []string{"--path=http://pd-service"},
+				},
+			},
+			RestartPolicy: corev1.RestartPolicyOnFailure,
+		},
+	}
+}
