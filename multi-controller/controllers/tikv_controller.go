17b5d68aa810ccfd66f181c763ad3634014b3ba8
troyyxk <xingkunyin@hotmail.com>
2022-11-22 17:34:13 -0500

    add tikv controller

--- multi-controller/controllers/tikv_controller.go
+++ multi-controller/controllers/tikv_controller.go
@@ -19,19 +19,29 @@ package controllers
 import (
 	"context"
 
+	"time"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	corev1 "k8s.io/api/core/v1"
+
 	tidbclusterv1 "cluster-operator/api/v1"
+	"cluster-operator/pkg/spawn"
 )
 
 // TikvReconciler reconciles a Tikv object
 type TikvReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	logger logr.Logger
 }
+
+const defaultTikvImage string = "pingcap/tikv:latest"
 
 //+kubebuilder:rbac:groups=tidb-cluster.dbaas,resources=tikvs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=tidb-cluster.dbaas,resources=tikvs/status,verbs=get;update;patch
@@ -47,9 +57,126 @@ type TikvReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *TikvReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	r.logger = log.FromContext(ctx)
+	r.logger.Info("Tikv reconcile")
 
 	// TODO(user): your logic here
+	instance := &tidbclusterv1.Tikv{}
+	err := r.Get(context.TODO(), req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// tikv not found, could have been deleted after
+			// reconcile request, don't requeue
+			r.logger.Info("TiKV instance not found, could be deleted, NOT REQUEUED")
+			return ctrl.Result{}, nil
+		}
+
+		// error reading the object, requeue the request
+		r.logger.Info("TiKV instance read err, REQUEUED")
+		return ctrl.Result{}, err
+	}
+
+	if instance.Status.Phase == "" {
+		instance.Status.Phase = tidbclusterv1.PhasePending
+	}
+
+	if instance.Spec.Imagename == "" {
+		instance.Spec.Imagename = defaultTikvImage
+	}
+
+	if instance.Spec.HealthCheckInterval == 0 {
+		instance.Spec.HealthCheckInterval = 5
+	}
+
+	switch instance.Status.Phase {
+	case tidbclusterv1.PhasePending:
+		//Check if pd is running
+		if !r.isPdRunning(ctx, req) {
+			r.logger.Info("Pd instance not ready")
+			return ctrl.Result{RequeueAfter: time.Duration(instance.Spec.HealthCheckInterval) * time.Second}, nil
+		} else {
+			r.logger.Info("Phase: Pd is running, Tikv PENDING for creation, now will create")
+			instance.Status.Phase = tidbclusterv1.PhaseCreating
+		}
+	case tidbclusterv1.PhaseCreating:
+		r.logger.Info("Phase: CREATING")
+		pod := spawn.NewTikvPod(instance)
+		err := ctrl.SetControllerReference(instance, pod, r.Scheme)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		tikvPod := &corev1.Pod{}
+
+		//Check if pod already exist
+		err = r.Get(context.TODO(), req.NamespacedName, tikvPod)
+		if err != nil && errors.IsNotFound(err) {
+			//Pod does not exist create the pod
+			err = r.Create(context.TODO(), pod)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			r.logger.Info("Pod Created successfully", "name", pod.Name)
+			instance.Status.Phase = tidbclusterv1.PhaseRunning
+			err = r.UpdateInstanceStatus(&ctx, instance)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			return ctrl.Result{RequeueAfter: time.Duration(instance.Spec.HealthCheckInterval) * time.Second}, nil
+		} else if err != nil {
+			// requeue with err
+			r.logger.Error(err, "cannot create pod")
+			return ctrl.Result{}, err
+		} else if tikvPod.Status.Phase == corev1.PodFailed {
+			// pod errored out, need to recreate
+			r.logger.Info("TiKV pod failed", "reason", tikvPod.Status.Reason, "message", tikvPod.Status.Message)
+			// delete failed pod
+			err = r.Delete(context.TODO(), tikvPod)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			instance.Status.Phase = tidbclusterv1.PhasePending
+		} else {
+			//Pod already exist and running, do nothing
+			return ctrl.Result{}, nil
+		}
+	case tidbclusterv1.PhaseRunning:
+		r.logger.Info("Phase: RUNNING")
+
+		tikvPod := &corev1.Pod{}
+
+		//Get current pod
+		err = r.Get(context.TODO(), req.NamespacedName, tikvPod)
+		if err != nil && errors.IsNotFound(err) {
+			r.logger.Info("TiKV pod disappeared in RUNNING phase, return to PENDING")
+			instance.Status.Phase = tidbclusterv1.PhasePending
+		} else if err != nil {
+			// requeue with err
+			r.logger.Error(err, "cannot create TiKV pod")
+			return ctrl.Result{}, err
+		} else if tikvPod.Status.Phase == corev1.PodFailed {
+			// pod errored out, need to recreate
+			r.logger.Info("TiKV pod failed", "reason", tikvPod.Status.Reason, "message", tikvPod.Status.Message)
+			// delete failed pod
+			err = r.Delete(context.TODO(), tikvPod)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			instance.Status.Phase = tidbclusterv1.PhasePending
+		} else {
+			//Pod already exist and running, do nothing
+			return ctrl.Result{RequeueAfter: time.Duration(instance.Spec.HealthCheckInterval) * time.Second}, nil
+		}
+	default:
+		r.logger.Info("ERROR: Unknown phase")
+		return ctrl.Result{}, nil
+	}
+
+	// update status
+	err = r.UpdateInstanceStatus(&ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -59,4 +186,27 @@ func (r *TikvReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&tidbclusterv1.Tikv{}).
 		Complete(r)
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *TikvReconciler) UpdateInstanceStatus(ctx *context.Context, instance *tidbclusterv1.Tikv) error {
+	return r.Status().Update(context.TODO(), instance)
+}
+
+// Check pd running
+func (r *TikvReconciler) isPdRunning(ctx context.Context, req ctrl.Request) bool {
+	pdInstance := &tidbclusterv1.Pd{}
+	err := r.Get(context.TODO(), req.NamespacedName, pdInstance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			r.logger.Info("Pd instance not found")
+		} else {
+			r.logger.Info("Error reading pd instance")
+		}
+		return false
+	} else if pdInstance.Status.Phase != tidbclusterv1.PhaseRunning {
+		r.logger.Info("Pd instance not running")
+		return false
+	}
+	return true
 }
---
+++ multi-controller/pkg/spawn/TikvPod.go
@@ -0,0 +1,42 @@
+package spawn
+
+import (
+	tidbclusterv1 "cluster-operator/api/v1"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func NewTikvPod(cr *tidbclusterv1.Tikv) *corev1.Pod {
+	labels := map[string]string{
+		"app": cr.Name,
+	}
+
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name,
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "tikv",
+					Image: cr.Spec.Imagename,
+					Ports: []corev1.ContainerPort{
+						{
+							Name:          "sql-endpoint",
+							ContainerPort: 4000,
+						},
+						{
+							Name:          "monitoring",
+							ContainerPort: 10080,
+						},
+					},
+					Command: []string{"/tikv-server", "--path=pd-svc:2379"},
+				},
+			},
+			RestartPolicy: corev1.RestartPolicyOnFailure,
+		},
+	}
+}
