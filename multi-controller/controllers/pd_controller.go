29dc1f8c484802849548bfd1313d7b4e2012931d
Jingyu Su <jingyusu@bu.edu>
2022-11-22 20:26:18 -0500

    pd controller

--- multi-controller/controllers/pd_controller.go
+++ multi-controller/controllers/pd_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -94,24 +95,129 @@ func (r *PdReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Re
 		if err != nil {
 			return ctrl.Result{}, err
 		}
-		r.logger.Info("PD svc created successfully")
 
 		pdExistingSVC := &corev1.Service{}
 
 		//Check if service already exist
 		err = r.Get(context.TODO(), req.NamespacedName, pdExistingSVC)
 
-		if err != nil {
+		if err != nil && errors.IsNotFound(err) {
 			//svc does not exist
-			if errors.IsNotFound(err) {
-				err = r.Create(context.TODO(), pdSVC)
-			}
-
+			err = r.Create(context.TODO(), pdSVC)
 			//failed to create svc, requeue with error
 			if err != nil {
 				return ctrl.Result{}, err
 			}
+			r.logger.Info("PD SVC Created successfully", "name", pdSVC.Name)
+			pdInstance.Status.Phase = tidbclusterv1.PhaseCreating_PD_Pod
+			err = r.updatePDInstanceStatus(&ctx, pdInstance)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			//Start creating pod after 1 second
+			return ctrl.Result{RequeueAfter: time.Second}, nil
+		} else if err != nil {
+			// requeue with err
+			r.logger.Error(err, "cannot create pd svc")
+			return ctrl.Result{}, err
+		} else {
+			// Service exist, proceed to pod creation
+			pdInstance.Status.Phase = tidbclusterv1.PhaseCreating_PD_Pod
 		}
+	case tidbclusterv1.PhaseCreating_PD_Pod:
+		r.logger.Info("Phase: PD CREATING Pod")
+		pdPod := spawn.NewPdPod(pdInstance)
+		err := ctrl.SetControllerReference(pdInstance, pdPod, r.Scheme)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		pdExistingPod := &corev1.Pod{}
+
+		//Check if pod already exist
+		err = r.Get(context.TODO(), req.NamespacedName, pdExistingPod)
+
+		if err != nil && errors.IsNotFound(err) {
+			//pod does not exist
+			err = r.Create(context.TODO(), pdPod)
+			//failed to create pod, requeue with error
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			r.logger.Info("PD Pod Created successfully", "name", pdPod.Name)
+			pdInstance.Status.Phase = tidbclusterv1.PhaseRunning_PD
+			err = r.updatePDInstanceStatus(&ctx, pdInstance)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			//Start creating pod after health check interval
+			return ctrl.Result{RequeueAfter: time.Duration(pdInstance.Spec.HealthCheckInterval) * time.Second}, nil
+		} else if err != nil {
+			// requeue with err
+			r.logger.Error(err, "cannot create pd pod, REQUEUE")
+			return ctrl.Result{}, err
+		} else if pdExistingPod.Status.Phase == corev1.PodFailed {
+			// pod errored out, need to recreate
+			r.logger.Info("PD pod failed", "reason", pdExistingPod.Status.Reason, "message", pdExistingPod.Status.Message)
+			// delete failed pod
+			err = r.Delete(context.TODO(), pdExistingPod)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			pdInstance.Status.Phase = tidbclusterv1.PhaseCreating_PD_Pod
+		} else {
+			//Pod already exist and running, do nothing
+			return ctrl.Result{}, nil
+		}
+	case tidbclusterv1.PhaseRunning_PD:
+		r.logger.Info("Phase: PD RUNNING")
+
+		pdPod := &corev1.Pod{}
+
+		//Get current pod
+		err = r.Get(context.TODO(), req.NamespacedName, pdPod)
+		if err != nil && errors.IsNotFound(err) {
+			//Smh the pod disappeared create the pod
+			/*
+				pod := spawn.NewTidbPod(instance)
+				err := ctrl.SetControllerReference(instance, pod, r.Scheme)
+				if err != nil {
+					return ctrl.Result{}, err
+				}
+				err = r.Create(context.TODO(), pod)
+				if err != nil {
+					return ctrl.Result{}, err
+				}
+				logger.Info("Pod Created successfully", "name", pod.Name)
+				instance.Status.Phase = tidbclusterv1.PhaseRunning
+				err = r.UpdateInstanceStatus(&ctx, instance)
+				if err != nil {
+					return ctrl.Result{}, err
+				}
+				return ctrl.Result{RequeueAfter: time.Duration(instance.Spec.HealthCheckInterval) * time.Second}, nil
+			*/
+			r.logger.Info("PD pod disappeared in RUNNING phase, return to POD creating")
+			pdInstance.Status.Phase = tidbclusterv1.PhaseCreating_PD_Pod
+		} else if err != nil {
+			// requeue with err
+			r.logger.Error(err, "cannot create PD pod")
+			return ctrl.Result{}, err
+		} else if pdPod.Status.Phase == corev1.PodFailed {
+			// pod errored out, need to recreate
+			r.logger.Info("PD pod failed", "reason", pdPod.Status.Reason, "message", pdPod.Status.Message)
+			// delete failed pod
+			err = r.Delete(context.TODO(), pdPod)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			pdInstance.Status.Phase = tidbclusterv1.PhaseCreating_PD_Pod
+		} else {
+			//Pod already exist and running, do nothing
+			return ctrl.Result{RequeueAfter: time.Duration(pdInstance.Spec.HealthCheckInterval) * time.Second}, nil
+		}
+	default:
+		r.logger.Info("ERROR: Unknown phase")
+		return ctrl.Result{}, nil
 	}
 	//TODO: think about service creation logic
 	return ctrl.Result{}, nil
@@ -122,4 +228,9 @@ func (r *PdReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&tidbclusterv1.Pd{}).
 		Complete(r)
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *PdReconciler) updatePDInstanceStatus(ctx *context.Context, pdInstance *tidbclusterv1.Pd) error {
+	return r.Status().Update(context.TODO(), pdInstance)
 }
