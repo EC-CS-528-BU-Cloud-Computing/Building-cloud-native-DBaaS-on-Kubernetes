27d8b089d35b547a01bf5022b73ea0754aba9a0f
Jingyu <gar123su@gmail.com>
2022-12-05 15:46:56 -0500

    prometheus installation script

---
+++ multi-controller/config/samples/prometheusDepl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+helm install prometheus prometheus-community/prometheus
