bbe24825ec89459e6ac8e6f5c52a4eb0ba638839
troyyxk <39604949+troyyxk@users.noreply.github.com>
2022-12-10 19:18:42 -0500

    Update prometheusDepl.sh

--- multi-controller/config/samples/prometheusDepl.sh
+++ multi-controller/config/samples/prometheusDepl.sh
@@ -4,3 +4,4 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 helm install prometheus prometheus-community/prometheus
+helm install grafana grafana/grafana-agent-operator
